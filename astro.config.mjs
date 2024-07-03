152beb79dbb70fc72fcb73391379851a5099b3d2
Taglianini <tomastagliani1234@gmail.com>
2024-07-03 10:47:04 -0300

    Version final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tagliani.netlify.app"
+  site: "https://tagliani.netlify.app",
+  integrations: [preact()]
 });
-
